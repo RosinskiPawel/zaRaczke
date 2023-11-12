68c154b6a70651b77328089d734493e315952e0a
Pawel <100613143+RosinskiPawel@users.noreply.github.com>
2023-11-12 19:16:30 +0100

    pawn and the end of the board

--- warcaby/classes.py
+++ warcaby/classes.py
@@ -31,45 +31,76 @@ class Warcaby:
             print(variable, "[", *value, "]")
 
     def move(self):
+        player = 1
         while True:
             letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
-
-            # ustalamy pionek do przeniesienia
-            # pionek w przypadku gracza pierwsze to '1', a drugiego to '2' zostaje pobrany/chwycony i jego pole jest puste i zmienia teraz wartość na '0', ale wartość pionek pozostaje niezmieniona i zostanie użyta w dalszej części
-            # obsługa błędu w przypadku próby określenia pionka spoza dostępnego zakresu
             while True:
+                print(f"Gracz nr {1 if player%2 else 2}")
+                # ustalamy pionek do chwycenia przez określenie rzędu i kolumny,
+
                 try:
-                    get_row = input("podaj rząd: ")
+                    get_row = input("Podaj rząd: ")
                     get_col = int(input("Podaj kolumnę: "))
+                    # obsługa błędów w przypadku wyjścia poza zakres a-h oraz 1-8
                     if get_row in letters and get_col in range(1, 9):
                         break
                     else:
-                        print("błędna wartości")
+                        print("Błędna wartość")
                 except ValueError:
-                    print("spróuj ponownie")
-
+                    print("Spróbuj ponownie")
+            # określana wartość pionka: 1 lub 2
             pawn = self.board[letters.index(get_row)][get_col - 1]
-
+            # w miejsce pobranego pionka zostaje wpisane 0
             self.board[letters.index(get_row)][get_col - 1] = 0
-            # teraz ustalamy miejsce, na ktore przenosimy pobrany pionek ('1' lub '2') - z obsługą błędu
 
+            # teraz ustalamy miejsce, na ktore przenosimy pobrany pionek ('1' lub '2') - z obsługą błędów
             while True:
                 try:
-                    set_row = input("podaj rząd docelowy: ")
+                    set_row = input("Podaj rząd docelowy: ")
                     set_col = int(input("Podaj kolumnę docelową: "))
+                    # obsługa błędu przekroczenia zakresu a-h/1-8
                     if set_row in letters and set_col in range(1, 9):
+                        # obsługa błędu przeniesienia pionka na pole zajęte przez inny pionek
                         if self.board[letters.index(set_row)][set_col - 1] == 0:
-                            break
+                            # obsługa błędu ruchu do tyłu
+                            if (
+                                letters.index(set_row) > letters.index(get_row)
+                                and pawn == 1
+                                or letters.index(set_row) < letters.index(get_row)
+                                and pawn == 2
+                            ):
+                                break
+                            else:
+                                print("Nie wolno poruszać się do tyłu!")
                         else:
                             print("To pole jest zajęte. Wybierz inne!")
                     else:
-                        print("błędna wartość")
+                        print("Błędna wartość")
                 except ValueError:
-                    print("błędna wartości, spróuj ponownie")
-            self.board[letters.index(set_row)][set_col - 1] = pawn
-            # wartosc na nowym miejscu zmienia się na wartość pionka pobranego/chwyconego wyzej
+                    print("Błędna wartość, spróbuj ponownie")
+
+            # gdy pionek dojdzie do końca planszy, wówczas powraca na początek
+            # gdy przy pionku '2' indeks docleowego rzędu  wynosi 0, wówczas pionek dociera do końca planszy,
+            # wartość w tym miejscu przyjmuje 0 i pojawia się pytanie do jakiej kolumny wstawić '2' w rzędzie H o indeksie 7
+            # i pionek jest ponownie dostepny do gry
+            # w przypadku pionka '1' zmieniają się tylko indeksy, zasada taka sama
+            # gdy nie zachodzi żaden z obu warunków, w zwykłej sytuacji w wybrane miejsce na planszy
+            # zostaje umieszony pionek o określonej wyżej wartości 1 lub 2
+            if pawn == 2 and letters.index(set_row) == 0:
+                self.board[0][set_col - 1] = 0
+                self.board[7][int(input("W której kolumnie umieścić pionek?")) - 1] = 2
+            elif pawn == 1 and letters.index(set_row) == 7:
+                self.board[7][set_col - 1] = 0
+                self.board[0][int(input("W której kolumnie umieścić pionek?")) - 1] = 1
+            else:
+                self.board[letters.index(set_row)][set_col - 1] = pawn
 
             # ZBIJANIE!!!!
+            # oto pierwotny kształt ustalania warunku bicia.
+            # Bicie jest wówczas, gdy różnica między wartościami indeksów kolumn i wierszy wynosi 2/-2.
+            # pionek między aktualną pozycją pionka gracza zbijającego a jego pozycją docelową zmienia wartość na zero,
+            # określa się go przez odejmowanie lub dodawanie 1 lub 2 do wartości indeksów.
+
             # if (
             #     get_col - set_col == -2
             #     and letters.index(get_row) - letters.index(set_row) == -2
@@ -91,6 +122,7 @@ class Warcaby:
             # ):
             #     self.board[letters.index(get_row) - 1][get_col] = 0
 
+            # to jest uproszczona i bardziej kompaktowa wersja bicia
             if (
                 abs(get_col - set_col) == 2
                 and abs(letters.index(get_row) - letters.index(set_row)) == 2
@@ -101,6 +133,7 @@ class Warcaby:
                 col = set_col + (-2 if get_col < set_col else 0)
                 self.board[row][col] = 0
 
+            # Dodawanie punktów
             if abs(get_col - set_col) == 2 and pawn == 1:
                 self.scoreOne += 1
             elif abs(get_col - set_col) == 2 and pawn == 2:
@@ -109,17 +142,15 @@ class Warcaby:
             print()
             self.view()
             print(f"Gracz 1 ma {self.scoreOne}. Gracz 2 ma {self.scoreTwo}")
+            player += 1
+
+            # wygrywa gracz, który pierwszy zdobędzie 12 punktów
             if self.scoreOne == 12:
-                print("wygał gracz pierwszy")
+                print("Wygrał gracz pierwszy!")
             elif self.scoreTwo == 12:
-                print("wybrał gracz drugi")
-                question = input("chcesz zagrać ponownie? (y/n)")
+                print("Wygrał gracz drugi!")
+                question = input("Chcesz zagrać ponownie? (y/n)")
                 if question == "y":
                     return True
                 else:
                     break
-
-        # TODO 1) obsługa błędów (pole zajęte, za duży skok, wyjście poza zasięg tablicy)
-
-
-# może by tę funkcję rozbić na mniejsze - chyba bedzie konieczne, bo musi dojść jeszcz BICIE
