0bd5731463f6e002573a7c90b5cfa9f4b25f1e78
Pawel <100613143+RosinskiPawel@users.noreply.github.com>
2023-12-02 20:11:57 +0100

    modified extracting_data method

--- PDFreader/modules.py
+++ PDFreader/modules.py
@@ -20,30 +20,30 @@ class MyOrdersReader:
 
         self.btn_open = Button(
             root,
-            text="Select folder to open",
+            text="Select files to open",
             bg="#EEE9BF",
             font=("Courier,15,bold"),
             command=self.openFiles,
         )
         self.btn_open.pack()
 
-        self.btn2_save = Button(
-            root,
-            text="Select file to save",
-            bg="#EEE9BF",
-            font=("Courier,15,bold"),
-            command=self.fileToSaveIn,
-        )
-        self.btn2_save.pack()
-
-        self.btn3_convert = Button(
+        self.btn2_convert = Button(
             root,
             text="Convert",
             bg="#EEE9BF",
             font=("Courier,15,bold"),
             command=self.action,
         )
-        self.btn3_convert.pack()
+        self.btn2_convert.pack()
+
+        self.btn3_save = Button(
+            root,
+            text="Select file to save",
+            bg="#EEE9BF",
+            font=("Courier,15,bold"),
+            command=self.fileToSaveIn,
+        )
+        self.btn3_save.pack()
 
         self.btn4_exit = Button(
             root, text="Exit", bg="#EEE9BF", font=("Courier,15,bold"), command=exit
@@ -51,39 +51,35 @@ class MyOrdersReader:
         self.btn4_exit.pack()
 
     def openFiles(self):
+        # utworzenie listy plików, które zostaną otwarte z katalogu
         self.chosen_files = filedialog.askopenfilenames(
             defaultextension=".pdf",
             filetypes=[("Pliki PDF", "*.pdf"), ("Wszystkie pliki", "*.*")],
         )
 
     def action(self):
+        # działanie po naciśnięciu przycisku Convert
         self.gettingFirstPages()
         self.converting()
 
     def gettingFirstPages(self):
-        # filesList = ("C:/CODING/pdfToRead.pdf", "C:/CODING/pdfToRead2.pdf")
-        # lista list z przekonwertowanymi pierwszymi stronami plików pdf
-
-        # dla kazdego pliku z listy otwartych plikow
-        # for file in filesList:
+        # tworzenie list z przekonwertowanymi pierwszymi stronami plików pdf
         for file in self.chosen_files:
             pdfPath = Path(file)
             pdf_file = PdfReader(str(pdfPath))
             main_page = pdf_file.pages[0]
             # konwersja na tekst
             converted_page = main_page.extract_text()
+
             # z uwagi na trudne formatowanie tekstu zostaje on zamieniony na liste, a elementy sa dzielone na podstawie znaku nowego wiersza \n
             page_as_list = converted_page.split("\n")
 
-            #     for el in page_as_list:
-            #         page_as_list.remove(el) if el == " " else el
-            #         list_pages.append(page_as_list)
-            # print(list_pages)
             # usuwanie pustych elementow, aby bylo latwiej okreslac indeksy
             cleaned_list = [el.strip() for el in page_as_list if el.strip()]
+            print(cleaned_list)
+
             # dodwanie do listy oczyszczonych list
             self.list_pages.append(cleaned_list)
-
         return self.list_pages
 
     def converting(self):
@@ -100,38 +96,39 @@ class MyOrdersReader:
         return self.pure_info
 
     def extracting_pm(self, page):
+        # szukanie nazwiska PM
         pm_index = page.index("PROJEKTLEITER")
         pm = page[pm_index + 1]
         return pm
 
     def extracting_num(self, page):
+        # szukanie numeru zamówienia PO
         num_index = page.index("PROJEKTNUMMER")
         num = page[num_index + 1]
         return num
 
     def extracting_data(self, page):
+        # szukanie daty PO i zmiana formatu z "dd. month po niemiecku yyyy" na 'dd.mm.yyyy'
         date_index = page.index("LIEFERTERMIN")
+
+        if page[date_index + 1] == "BEMERKUNG ZUM LIEFERTERMIN":
+            date_index = page.index("AUFTRAG ERTEILT AM")
+
         projectDateTemp = page[date_index + 1].split(", ")
         data_object = datetime.strptime(projectDateTemp[1], "%d. %B %Y")
         projectDate = data_object.strftime("%d.%m.%Y")
         return projectDate
 
     def extracting_value(self, page):
+        # tworzy listę wartości z końcówką "€" i następnie wybiera z nich największą, która stanowi łączna wartość zamówienia"
         proj_value = []
         for el in page:
             if el.endswith("€"):
                 proj_value.append(float(el.removesuffix("€").replace(",", ".")))
         return max(proj_value)
 
-    # def extracting_value(self):
-    #     proj_value = []
-    #     for page in self.list_pages:
-    #         for el in page:
-    #             if el.endswith("€"):
-    #                 proj_value.append(float(el.removesuffix("€").replace(",", ".")))
-    #     return proj_value
-
     def fileToSaveIn(self):
+        # wybór pliku do zapisu
         fileName = filedialog.askopenfilename(
             defaultextension=".txt",
             filetypes=[("Pliki arkusza", "*.txt"), ("Wszystkie pliki", "*.*")],
@@ -141,110 +138,6 @@ class MyOrdersReader:
             print(f"dane zostały zapisane w plik {fileName}")
 
 
-# app = MyOrdersReader()
-
-
-# def main():
-#     app.gettingFirstPages()
-#     app.converting()
-
-
-# main()
-
 root = Tk()
 app = MyOrdersReader(root)
 root.mainloop()
-
-
-# root = Tk()
-# root.geometry("500x300")
-# root.resizable(False, False)
-# root.title("PDF to Excel")
-# root.config(bg="#EEE9BF")
-
-# lb = Label(root, text="PO's Reader and Convert", bg="#EEE9BF", font=("Arial,15,bold"))
-# lb.pack(pady=10)
-
-
-# btn1 = Button(
-#     root,
-#     text="Select folder to open",
-#     bg="#EEE9BF",
-#     font=("Courier,15,bold"),
-#     command=openFiles,
-# )
-# btn1.place(x=140, y=70)
-# btn2 = Button(
-#     root,
-#     text="Select file to save",
-#     bg="#EEE9BF",
-#     font=("Courier,15,bold"),
-#     command=fileToSaveIn,
-# )
-# btn2.place(x=140, y=120)
-
-# btn3 = Button(
-#     root,
-#     text="Convert",
-#     bg="#EEE9BF",
-#     font=("Courier,15,bold"),
-#     command=gettingFirstPages,
-# )
-# btn3.place(x=140, y=200)
-# btn4 = Button(root, text="Exit", bg="#EEE9BF", font=("Courier,15,bold"), command=exit)
-# btn4.place(x=145, y=250)
-# root.mainloop()
-
-
-# def exit(self):
-#     self.root.destroy()
-
-
-# # wczytuje plik pdf
-# pdf_path = Path("C:/CODING/pdfToRead2.pdf")
-# pdf = PdfReader(str(pdf_path))
-# # pobiera 1.stronę
-# first_page = pdf.pages[0]
-# # konwertuje na tekst
-# pageToWord = first_page.extract_text()
-
-# # tekst ma wiele lini konca wiersza, więc tworzona jest lista z podziałem konca wiersza '\n'
-# pageL = pageToWord.split("\n")
-# # usuwanie pustych miejsc
-# for el in pageL:
-#     pageL.remove(el) if el == " " else el
-# print(pageL)
-
-
-# # # indeksy dla słow kluczowych
-
-# projectPM_index = pageL.index("PROJEKTLEITER")
-# projectNum_index = pageL.index("PROJEKTNUMMER")
-# projectDate_index = pageL.index("LIEFERTERMIN")
-# # projectValue_index = pageL.index("Honorar")
-# projValue = []
-# # tu szuka el z końcówą "€", tworzy listę wartości, usuwa "€", zamienia ',' na '.' i tworzy ze stringów liczby zmiennoprzecinkowe, aby wyszukać największą, która jest sumą w euro na zamówieniu PO
-# for el in pageL:
-#     if el.endswith("€"):
-#         projValue.append(float(el.removesuffix("€").replace(",", ".")))
-# print(projValue)
-# # wartości dla kluczy
-# projectPM = pageL[projectPM_index + 1]
-# projectNumber = pageL[projectNum_index + 1]
-# # projectValue = pageL[projectValue_index + 2]
-# projectValue = max(projValue)
-
-# # modyfikcja zapisu daty
-# projectDateTemp = pageL[projectDate_index + 1].split(", ")
-# data_object = datetime.strptime(projectDateTemp[1], "%d. %B %Y")
-# projectDate = data_object.strftime("%d.%m.%Y")
-
-
-# print(
-#     f"Nr projektu: {projectNumber}; PM: {projectPM}; data: {projectDate}, wartość {projectValue}"
-# )
-
-
-# # first = Invoice(122, 34, "DF", 23112023, 150)
-# # print(first.name)
-# # print(first.toList()[3])
