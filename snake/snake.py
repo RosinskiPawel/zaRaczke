d6a957df621c59c3280ddea7c9f3ee7d58ca1a36
Pawel <100613143+RosinskiPawel@users.noreply.github.com>
2024-02-12 00:22:03 +0100

    modified out_of_window

--- snake/snake.py
+++ snake/snake.py
@@ -29,18 +29,6 @@ pygame.display.set_caption("SNAKE")
  
 
 
-class Food:
-    def __init__(self):
-        # randomowa pozycja w zakresie 0 do krawedzi - rozmiar elementu i dodatkowo krok wielkości elementu, aby food miescił się w kratce
-
-        self.x = random.randrange(0, BOARD_SIZE - ELEMENT_SIZE, ELEMENT_SIZE)
-        self.y = random.randrange(0, BOARD_SIZE - ELEMENT_SIZE, ELEMENT_SIZE)
-        self.rect = pygame.Rect(self.x, self.y, ELEMENT_SIZE, ELEMENT_SIZE)
-        self.color = random.choice(NEON_COLORS)
-
-    def random_position(self):
-        pygame.draw.rect(screen, self.color, self.rect, 5)
-
 
 class Snake:
     def __init__(self):
@@ -91,67 +79,78 @@ class Snake:
 
     # head out of game's board
     def out_of_window(self):
-        if snake.head_position[0] not in range(0, BOARD_SIZE) or snake.head_position[
+        if snake.head_position[0] not in range(0, BOARD_SIZE+ELEMENT_SIZE) or snake.head_position[
             1
-        ] not in range(0, BOARD_SIZE):
+        ] not in range(0, BOARD_SIZE+ELEMENT_SIZE):
             self.gameOver()
-           
-            
+class Food:
+    def __init__(self):
+        # randomowa pozycja w zakresie 0 do krawedzi - rozmiar elementu i dodatkowo krok wielkości elementu, aby food miescił się w kratce
+
+        self.x = random.randrange(0, BOARD_SIZE - ELEMENT_SIZE, ELEMENT_SIZE)
+        self.y = random.randrange(0, BOARD_SIZE - ELEMENT_SIZE, ELEMENT_SIZE)
+        self.rect = pygame.Rect(self.x, self.y, ELEMENT_SIZE, ELEMENT_SIZE)
+        self.color = random.choice(NEON_COLORS)
+
+    def random_position(self):
+        pygame.draw.rect(screen, self.color, self.rect, 5)
+
+      
+food = Food()
+snake = Snake()
+counter = 0
+
 while True:
-    food = Food()
-    snake = Snake()
-    counter = 0
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-            # zmiana kierunku
-            if event.type == pygame.KEYDOWN:
-                # warunek 'direction !=' zabezpiecza weza przed odwrócneiem się i zjedzeniem ogona
-                if event.key == pygame.K_DOWN and snake.direction != "up":
-                    snake.direction = "down"
-                elif event.key == pygame.K_UP and snake.direction != "down":
-                    snake.direction = "up"
-                elif event.key == pygame.K_RIGHT and snake.direction != "left":
-                    snake.direction = "right"
-                elif event.key == pygame.K_LEFT and snake.direction != "right":
-                    snake.direction = "left"
-                    
-        # animacja ruchu
-        if snake.direction == "right":
-            snake.head_position[0] += ELEMENT_SIZE
-        if snake.direction == "up":
-            snake.head_position[1] -= ELEMENT_SIZE
-        if snake.direction == "down":
-            snake.head_position[1] += ELEMENT_SIZE
-        if snake.direction == "left":
-            snake.head_position[0] -= ELEMENT_SIZE
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        # zmiana kierunku
+        if event.type == pygame.KEYDOWN:
+            # warunek 'direction !=' zabezpiecza weza przed odwrócneiem się i zjedzeniem ogona
+            if event.key == pygame.K_DOWN and snake.direction != "up":
+                snake.direction = "down"
+            elif event.key == pygame.K_UP and snake.direction != "down":
+                snake.direction = "up"
+            elif event.key == pygame.K_RIGHT and snake.direction != "left":
+                snake.direction = "right"
+            elif event.key == pygame.K_LEFT and snake.direction != "right":
+                snake.direction = "left"
+                
+    # animacja ruchu
+    if snake.direction == "right":
+        snake.head_position[0] += ELEMENT_SIZE
+    if snake.direction == "up":
+        snake.head_position[1] -= ELEMENT_SIZE
+    if snake.direction == "down":
+        snake.head_position[1] += ELEMENT_SIZE
+    if snake.direction == "left":
+        snake.head_position[0] -= ELEMENT_SIZE
 
-        screen.fill(GROUND_COLOR)
-        snake.move()
-        snake.draw()
-        snake.collision_body()
-        snake.out_of_window()
-        food.random_position()
-        text = text_font.render(f"{counter}", True, text_color)
-        screen.blit(text, (BOARD_SIZE // 2 - text.get_width() // 2, 0))
+    screen.fill(GROUND_COLOR)
+    snake.move()
+    snake.draw()
+    snake.collision_body()
+    snake.out_of_window()
+    food.random_position()
+    text = text_font.render(f"{counter}", True, text_color)
+    screen.blit(text, (BOARD_SIZE // 2 - text.get_width() // 2, 0))
 
-        # eating
-        if snake.new_head.colliderect(food.rect):
-            snake.color = food.color
-            snake.body.append(
-                pygame.Rect(
-                    snake.new_head[0], snake.new_head[1], ELEMENT_SIZE, ELEMENT_SIZE
-                )
+    # eating
+    if snake.new_head.colliderect(food.rect):
+        snake.color = food.color
+        snake.body.append(
+            pygame.Rect(
+                snake.new_head[0], snake.new_head[1], ELEMENT_SIZE, ELEMENT_SIZE
             )
-            counter += 1
+        )
+        counter += 1
 
-            # zapobieganie pojawieniu sie jedzenia na wezu
-            while True:
-                food = Food()
-                if food.rect not in snake.body:
-                    break
+        # zapobieganie pojawieniu sie jedzenia na wezu
+        while True:
+            food = Food()
+            if food.rect not in snake.body:
+                break
 
-        pygame.display.update()
-        clock.tick(5)
+    pygame.display.update()
+    clock.tick(5)
